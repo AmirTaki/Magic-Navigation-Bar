456e462e74423f7cf2b60b37442e87ffa41193a5
amir <amirtaki623@gmail.com>
2025-08-28 22:24:31 +0330

    part 32

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const icondowns = document.querySelectorAll(".fa-chevron-down")
 
 items.forEach((item) => {
     item.addEventListener("click", (e) => {
-        
+        if(this.classList.contains('active')  || e.target.classList.contains('fa-chevron-donw')){
+            return;
+        }
     })
 })
