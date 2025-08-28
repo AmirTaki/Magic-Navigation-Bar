81156394aaf8a666a2f584f6c0a7e81889489769
amir <amirtaki623@gmail.com>
2025-08-28 22:23:24 +0330

    part 31

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 const items = document.querySelectorAll(".item")
 const action = document.getElementById("action")
 const icondowns = document.querySelectorAll(".fa-chevron-down")
+
+items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        
+    })
+})
